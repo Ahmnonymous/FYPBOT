25da56f0e10b76d1ef6d4ac56040fd82aaea3cf9
ahmnonymous <a4medqureshi8@gmail.com>
2024-04-24 15:34:25 +0500

    COMMIT

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-$(document).ready(function() {
-    $(".toggle-password").click(function() {
-      $(this).find('i').toggleClass("fa-eye fa-eye-slash");
-      var input = $($(this).attr("toggle"));
-      if (input.attr("type") === "password") {
-        input.attr("type", "text");
-      } else {
-        input.attr("type", "password");
-      }
-    });
-  });
+document.addEventListener('DOMContentLoaded', function () {
+    fetchNews();
+});
+
+function fetchNews() {
+    const apiKey = 'a2138d7d4ed34d8698b3b607415d3bc5';
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Assuming the response data structure matches your requirement
+            const newsItems = data.articles.map(article => {
+                return `
+                    <div class="carousel-item">
+                        <img src="${article.urlToImage}" class="d-block w-100" alt="${article.title}">
+                        <div class="carousel-caption">
+                            <h5>${article.title}</h5>
+                            <p>${article.description}</p>
+                        </div>
+                    </div>
+                `;
+            }).join('');
+
+            document.getElementById('newsItems').innerHTML = newsItems;
+            document.querySelector('.carousel-item').classList.add('active'); // Set the first item as active
+        })
+        .catch(error => {
+            console.error('Error fetching news:', error);
+        });
+}
