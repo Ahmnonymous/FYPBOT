22886b4fe7ca9e2f3ebbc636ff37752deb9e4e90
Ali-Haider-code1 <alihaiderawan1345@gmail.com>
2024-04-24 19:09:45 +0500

    Backend added for login and signup

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+main().catch((err) => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/Binance');
+    console.log('Data Base Connected!');
+}
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname));
+
+const userSchema = new mongoose.Schema({
+    userName: String,
+    Password: String,
+    BinanceKey: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Signup Api
+app.post('/signup', async (req, res) => {
+    try {
+        const userName = req.body.username;
+        const plainPassword = req.body.password;
+        const Binancekey = req.body.binanceApiKey;
+
+        const existingUser = await User.findOne({ userName });
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'User with the same userName already exists' });
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
+
+        // Create a new user with the hashed password
+        const user = new User({
+            userName: userName,
+            Password: hashedPassword,
+            BinanceKey: Binancekey
+        });
+
+        await user.save();
+        return res.status(200).json({ message: 'Sign Up successful' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred while registering the user' });
+    }
+});
+// Login Api
+app.post('/login', async (req, res) => {
+    try {
+        const userName = req.body.username;
+        const plainPassword = req.body.password;
+
+        const user = await User.findOne({ userName });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const passwordMatch = await bcrypt.compare(plainPassword, user.Password);
+
+        if (!passwordMatch) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        // Send a success response with a redirect URL
+        return res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred while logging in' });
+    }
+});
+app.get('/login', (req, res) => {
+    return res.redirect('LoginPage.html')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
